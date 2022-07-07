6f4222f9e8ec06bba33be942e7528a45dec179a0
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-06 19:35:59 -0700

    update fetcher to prompt when file exists

--- fetcher.js
+++ fetcher.js
@@ -1,6 +1,15 @@
 const request = require('request');
 const fs = require('fs');
 const chalk = require('chalk');
+const readline = require('readline');
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
 
 const error = chalk.bold.red;
 const success = chalk.bold.green;
@@ -11,11 +20,12 @@ const path = args[1];
 
 const fetcher = (url, path) => {
   request(url, (err, data, body) => {
-    if (err) {
-      console.log(err);
+    if (data.statusCode !== 200) {
+      console.log(error(`Oh no!`));
+      console.log(`Looks like there was an issue with the URL.\nUse the status code to see what went wrong.\nStatus code: ${data.statusCode}`);
     }
     if (data.statusCode === 200) {
-      fs.writeFile(path, body, err => {
+      fs.writeFile(path, body, {encoding: 'utf-8', flag: 'wx' }, err => {
         if (err) {
           if (err.code === 'ENOENT') {
             console.log('File not found. Double the path.');
@@ -24,14 +34,33 @@ const fetcher = (url, path) => {
             console.log('Permission denied. Double the path.');
           }
           if (err.code === 'EEXIST') {
-            console.log('File already exists. Double the path.');
+            rl.question('File already exists. Double the path.\nIf you want to overwrite the file, press "y" followed by enter.', answer => {
+              if (answer === 'y') {
+                fs.writeFile(path, body, {encoding: 'utf-8', flag: 'w' }, err => {
+                  if (err) {
+                    console.log(error(`Oh no!`));
+                    console.log(`Looks like there was an issue with the URL.\nUse the status code to see what went wrong.\nStatus code: ${data.statusCode}`);
+                  }
+                  if (!err) {
+                    console.log(success(`Success!`));
+                    console.log(`The file was saved to ${path}`);
+                  }
+            
+                });
+                rl.close();
+              } else {
+                rl.close();
+              }
+            });
+            
           }
         }
-        console.log(success('File saved!'));
-        console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+        if (!err) {
+          console.log(success(`Success!`));
+          console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+        }
       });
     }
-    console.log(error(`Oh no! Looks like there was an issue with the URL.\nUse the status code to see what went wrong.\nStatus code: ${data.statusCode}`));
   });
 };
 
